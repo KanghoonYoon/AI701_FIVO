c9783fbfdb827980641af5837f08a0357e862fe9
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-11-30 16:56:20 +0900

    fix utils

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,14 @@
 import os
 
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 def mk_dir(path):
 
     num = 0
     while 1:
 
-        dir_name = 'result/' + path + str(num)
-        if os.path.isdir():
+        dir_name = os.path.join(PROJECT_ROOT, 'result', path + str(num))
+        if os.path.isdir(dir_name):
             num += 1
         else:
             os.mkdir(dir_name)
